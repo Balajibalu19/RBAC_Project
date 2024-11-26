bfc9675f7e37e67fddff7964d1ada6a1be03be61
Balaji <20x51a0483@srecnandyal.edu.in>
2024-11-26 16:15:02 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { HashRouter } from 'react-router-dom';  // Import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>  {/* Wrap your App component with HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
@@ -16,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
